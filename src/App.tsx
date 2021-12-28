68922f35a73be8a342862553aa29995f0d15f725
pramod.ambekar@pegasusinfocorp.com-at-718927010107 <pramod.pegasus.com>
2021-12-28 19:11:57 +0530

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Pages/Header"
+import ButtonAppBar from "./Components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Route path="/" exact component={Header} />
+        <Route path="/button" exact component={ButtonAppBar}/>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
